28921e2fcb9ea019db4b018f45f7beb048502c5a
MathTigran <97700033+MathTigran@users.noreply.github.com>
2022-12-24 02:20:17 +0300

    Update new_youtube_bot.py

--- new_youtube_bot.py
+++ new_youtube_bot.py
@@ -27,7 +27,6 @@ def get_video_info(video_id):
 
 @dp.message_handler(commands='start')
 async def first_of_all(message):
-    print(message)
     await bot.send_message(message.chat.id, f'Привет {message["from"]["first_name"]}, я YouTube-бот,\nпомогу тебе скачать видео из ютуба и вывести статистику ')
     await bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEGwTVjk7ZWMCxnbgZGH5Mff-Fiu1Vz3QACeQUAAjbsGwWk_F_hIHgbZisE')
     await bot.send_message(message.chat.id, 'отправь ссылку на видео: ')
@@ -37,8 +36,6 @@ async def first_of_all(message):
 async def get_message_and_send_info(message):
     link = message.text
     yt = YouTube(link)
-    print(message)
-    print(yt)
     if message.text.startswith == 'https://www.youtube.com/' or 'https://youtu.be/':
         await bot.send_message(message.chat.id, f'Скачиваю видео: *{yt.title}* \n'
                                                 f'Автор : [{yt.author}]({yt.channel_id}', parse_mode='Markdown')
@@ -53,12 +50,29 @@ async def download_youtube_video(link, message, bot):
     stream = yt.streams.filter(progressive=True, file_extension='mp4')
     stream.get_highest_resolution().download(f'{download_folder}', f'{message.chat.id}_{yt.title}')
     with open(f'{download_folder}{message.chat.id}_{yt.title}', 'rb') as video:
-        publication_date = get_video_info(link.split('=')[1])['items'][0]['snippet']['publishedAt']
-        print(get_video_info(link.split('=')[1])['items'][0]['statistics'])
+        if 'youtu.be' in link:
+            print(link.split('.be/'))
+            publication_date = get_video_info(link.split('.be/')[1])['items'][0]['snippet']['publishedAt']
+            print(publication_date)
+            view_count = get_video_info(link.split('.be/')[1])['items'][0]['statistics']['viewCount']
+            like_count = get_video_info(link.split('.be/')[1])['items'][0]['statistics']['likeCount']
+            comment_count = get_video_info(link.split('.be/')[1])['items'][0]['statistics']['commentCount']
+        elif 'www.youtube.com' in link:
+            print(link.split('='))
+            if '&' in link.split('=')[1]:
+                link2 = link.split('=')[1].split('&')[0]
+                publication_date = get_video_info(link2)['items'][0]['snippet']['publishedAt']
+                print(get_video_info(link2)['items'][0]['statistics'])
+                view_count = get_video_info(link2)['items'][0]['statistics']['viewCount']
+                like_count = get_video_info(link2)['items'][0]['statistics']['likeCount']
+                comment_count = get_video_info(link2)['items'][0]['statistics']['commentCount']
+            else:
+                publication_date = get_video_info(link.split('=')[1])['items'][0]['snippet']['publishedAt']
+                print(get_video_info(link.split('=')[1])['items'][0]['statistics'])
+                view_count = get_video_info(link.split('=')[1])['items'][0]['statistics']['viewCount']
+                like_count = get_video_info(link.split('=')[1])['items'][0]['statistics']['likeCount']
+                comment_count = get_video_info(link.split('=')[1])['items'][0]['statistics']['commentCount']
         menu = InlineKeyboardMarkup(row_width=2)
-        view_count = get_video_info(link.split('=')[1])['items'][0]['statistics']['viewCount']
-        like_count = get_video_info(link.split('=')[1])['items'][0]['statistics']['likeCount']
-        comment_count = get_video_info(link.split('=')[1])['items'][0]['statistics']['commentCount']
         viewers = InlineKeyboardButton(text=f'Просмотры: {view_count}', callback_data='viewers')
         comments = InlineKeyboardButton(text=f'Комментарии: {comment_count}', callback_data='comments')
         likes = InlineKeyboardButton(text=f'Лайки: {like_count}', callback_data='likes')
@@ -69,8 +83,8 @@ async def download_youtube_video(link, message, bot):
             await bot.send_video(message.chat.id, video, caption=f'Дата Выхода:*{publication_date}*', parse_mode='Markdown', reply_markup=menu)
         except Exception as e:
             upload_file = open(f'{download_folder}{message.chat.id}_{yt.title}', 'rb')
-
-            await bot.send_message(message.chat.id, 'К сожалению Видео слишком тяжелое, телеграм такой вес не поддерживает :))')
+            await bot.send_message(message.chat.id, 'К сожалению Видео слишком тяжелое, телеграм такой вес не поддерживает :)), '
+                                                    f'\n но я любезно возвращаю ссылку: {link}')
             await bot.send_message(message.chat.id, f'Вот статистика: \n'
                                                     f'Просмотры: {view_count} \n'
                                                     f'Комментарии: {comment_count} \n'
@@ -80,8 +94,9 @@ async def download_youtube_video(link, message, bot):
 async def send_weather_to_location(message: types.Message):
     latitude = message["location"]["latitude"]
     longitude = message["location"]["longitude"]
+    print(requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=minute,hourly&appid={weather_key}').json())
+
     weather = (requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={weather_key}').json())
-    print(weather)
     await bot.send_message(message.chat.id, f"Температура в данный момент: {round(weather['main']['temp'] - 273.15)}\n"
                                             f"Ощущается как: {round(weather['main']['feels_like'] - 273.15)}\n"
                                         f'Восход солнца в: {dt.datetime.fromtimestamp(weather["sys"]["sunrise"])} \n'
@@ -92,3 +107,4 @@ async def send_weather_to_location(message: types.Message):
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
