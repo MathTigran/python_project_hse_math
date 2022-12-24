eca0cb6e4aca386e845681ee5a1fcd777f0a824e
MathTigran <97700033+MathTigran@users.noreply.github.com>
2022-12-24 16:16:29 +0300

    Update new_youtube_bot.py

--- new_youtube_bot.py
+++ new_youtube_bot.py
@@ -5,14 +5,13 @@ from googleapiclient.discovery import build
 import json
 from time import sleep
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-import datetime as dt
+import datetime
 import requests
 
-
+download_folder = ''
 token = ''
 api = ''
 weather_key = ''
-download_folder = ''
 
 bot = Bot(token=token)
 dp = Dispatcher(bot)
@@ -29,21 +28,63 @@ def get_video_info(video_id):
 async def first_of_all(message):
     await bot.send_message(message.chat.id, f'Привет {message["from"]["first_name"]}, я YouTube-бот,\nпомогу тебе скачать видео из ютуба и вывести статистику ')
     await bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEGwTVjk7ZWMCxnbgZGH5Mff-Fiu1Vz3QACeQUAAjbsGwWk_F_hIHgbZisE')
-    await bot.send_message(message.chat.id, 'отправь ссылку на видео: ')
+    await bot.send_message(message.chat.id, 'отправь ссылку на видео: \n'
+                                            'или напиши город прогноз погоды которого ты хочешь увидеть (Москва, Лондон, Ереван) ')
 
 
-@dp.message_handler()
-async def get_message_and_send_info(message):
-    link = message.text
-    yt = YouTube(link)
-    if message.text.startswith == 'https://www.youtube.com/' or 'https://youtu.be/':
+@dp.message_handler(content_types='text')
+async def get_message_and_send_info(message: types.Message):
+    if 'https://www.youtube.com/' in message.text or 'https://youtu.be/' in message.text:
+        link = message.text
+        yt = YouTube(link)
         await bot.send_message(message.chat.id, f'Скачиваю видео: *{yt.title}* \n'
-                                                f'Автор : [{yt.author}]({yt.channel_id}', parse_mode='Markdown')
+                                            f'Автор : [{yt.author}]({yt.channel_id}', parse_mode='Markdown')
         await download_youtube_video(link, message, bot)
     else:
-        await bot.send_message(message.chat.id, f'хммм, не похоже на ссылку с YouTube.....')
-        await bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEGzl1jl7qCjejyxkchtpbxsyGrhm6nmAACjQMAAjbsGwU4uEquYGopGywE')
+        await bot.send_message(message.chat.id, f'Кажется ты просишь погоду, тогда получай погоду')
+        code_to_smile = {
+            "Clear": "Ясно \U00002600",
+            "Clouds": "Облачно \U00002601",
+            "Rain": "Дождь \U00002614",
+            "Drizzle": "Дождь \U00002614",
+            "Thunderstorm": "Гроза \U000026A1",
+            "Snow": "Снег \U0001F328",
+            "Mist": "Туман \U0001F32B"
+        }
 
+        try:
+            r = requests.get(
+                f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={weather_key}&units=metric"
+            )
+            data = r.json()
+
+            city = data["name"]
+            cur_weather = data["main"]["temp"]
+
+            weather_description = data["weather"][0]["main"]
+            if weather_description in code_to_smile:
+                wd = code_to_smile[weather_description]
+            else:
+                wd = "Посмотри в окно, не пойму что там за погода!"
+
+            humidity = data["main"]["humidity"]
+            pressure = data["main"]["pressure"]
+            wind = data["wind"]["speed"]
+            sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+            sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+            length_of_the_day = datetime.datetime.fromtimestamp(
+                data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+                data["sys"]["sunrise"])
+
+            await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+                                f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+                                f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+                                f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+                                f"***Хорошего дня!***"
+                                )
+        except Exception as e:
+            print(e)
+        await bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEGzl1jl7qCjejyxkchtpbxsyGrhm6nmAACjQMAAjbsGwU4uEquYGopGywE')
 
 async def download_youtube_video(link, message, bot):
     yt = YouTube(link)
@@ -90,21 +131,58 @@ async def download_youtube_video(link, message, bot):
                                                     f'Комментарии: {comment_count} \n'
                                                     f'Лайки: {like_count}')
 
+
 @dp.message_handler(content_types=['location'])
 async def send_weather_to_location(message: types.Message):
     latitude = message["location"]["latitude"]
     longitude = message["location"]["longitude"]
-    print(requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=minute,hourly&appid={weather_key}').json())
+    lang = 'ru'
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
 
-    weather = (requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={weather_key}').json())
-    await bot.send_message(message.chat.id, f"Температура в данный момент: {round(weather['main']['temp'] - 273.15)}\n"
-                                            f"Ощущается как: {round(weather['main']['feels_like'] - 273.15)}\n"
-                                        f'Восход солнца в: {dt.datetime.fromtimestamp(weather["sys"]["sunrise"])} \n'
-                                            f'Заход солнца в: {dt.datetime.fromtimestamp(weather["sys"]["sunset"])}')
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={weather_key}&units=metric"
+        )
+        data = r.json()
+        print(data)
 
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
 
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(
+            data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+                            f"Погода в районе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+                            f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+                            f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+                            f"***Хорошего дня!***"
+                            )
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
 
